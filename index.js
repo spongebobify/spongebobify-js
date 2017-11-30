f56e1c049bf773ff1664a164b078c3e48de67783
Benjamin Cooper <bencooper222@gmail.com>
2017-11-29 19:24:48 -0600

    Updated for browser compatibility

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-module.exports = function(text) {
-    let altered = "";
-    let isLower = true;
-    for (let i = 0; i < text.length; i++) {
-        altered += isLower ? text[i].toLowerCase() : text[i].toUpperCase();
-        isLower = !isLower;
-    }
-    return altered;
-}
+
+
+
+spongebobify = text => {
+  let altered = "";
+  let isLower = true;
+  for (let i = 0; i < text.length; i++) {
+    altered += isLower ? text[i].toLowerCase() : text[i].toUpperCase();
+    isLower = !isLower;
+  }
+  return altered;
+};
+
+
+module.exports = spongebobify;

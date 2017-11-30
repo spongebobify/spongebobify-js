f197dd024e9f9c5dcae880c267d0cfb76efa1d97
Benjamin Cooper <bencooper222@gmail.com>
2017-11-29 20:13:45 -0600

    Corrected file ignoring

---
+++ dist/spongebobify.js
@@ -0,0 +1,17 @@
+(function () {
+'use strict';
+
+spongebobify = text => {
+  let altered = "";
+  let isLower = true;
+  for (let i = 0; i < text.length; i++) {
+    altered += isLower ? text[i].toLowerCase() : text[i].toUpperCase();
+    isLower = !isLower;
+  }
+  return altered;
+};
+
+
+module.exports = spongebobify;
+
+}());

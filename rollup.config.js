d28c1b014f051a3ad6e14dbdea35d7f09f4c328d
Benjamin Cooper <bencooper222@gmail.com>
2017-12-02 15:19:48 -0600

    Added uglifying

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
+import uglify from 'rollup-plugin-uglify';
 
 export default {
-    input: 'src/main.ts',
-    name: 'spongebobify',
-    output: {
-        file: 'dist/spongebobify.js',
-        format: 'umd'
-    },
-    plugins: [typescript()]
+  input: "src/main.ts",
+  name: "spongebobify",
+  output: {
+    file: "dist/spongebobify.js",
+    format: "umd"
+  },
+  plugins: [
+    typescript(),
+    uglify({
+      mangle: {
+        keep_fnames: true
+      }
+    })
+  ]
 };

07ad9beec506eb3d0b2051fb4d8eb638539665c6
Benjamin Cooper <bencooper222@gmail.com>
2017-12-03 15:21:56 -0600

    Cleaned up code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 export default function spongebobify(text: string, startLower: boolean = true) {
-    return text
-        .split('')
-        .reduce(
-            (acc, el, i) =>
-                (acc +=
-                    !(i % 2) === startLower
-                        ? el.toLowerCase()
-                        : el.toUpperCase()),
-            ''
-        );
+  return text
+    .split("")
+    .reduce(
+      (acc, el, i) =>
+        (acc += !(i % 2) === startLower ? el.toLowerCase() : el.toUpperCase()),
+      ""
+    );
 }
